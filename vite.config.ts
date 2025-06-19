7d0675e6847e2f2ed4a8f7c2d5306f1914df677b
piseth2013 <128473317+piseth2013@users.noreply.github.com>
2025-06-19 13:55:07 +0700

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
-// vite.config.ts
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // No need to define input here
-})
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+  build: {
+    rollupOptions: {
+      input: 'index.html'
+      admin: 'admin/index.html'
+    }
+  }
+});
