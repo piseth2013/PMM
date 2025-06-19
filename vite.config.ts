ea733d510df018e1ada9f98ca9d5824e95c9462a
piseth2013 <128473317+piseth2013@users.noreply.github.com>
2025-06-19 13:26:59 +0700

    Fix Vite build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { fileURLToPath } from 'url';
-import path from 'path';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: path.resolve(__dirname),
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
