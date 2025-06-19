055d7db5855d6eaf5e47cd85b554731d6a3286ba
piseth2013 <128473317+piseth2013@users.noreply.github.com>
2025-06-19 15:18:59 +0700

    Updated LoginForm.tsx

--- src/components/Auth/LoginForm.tsx
+++ src/components/Auth/LoginForm.tsx
@@ -65,7 +65,7 @@ export function LoginForm() {
               <img
                 src="https://tzattbqdbdhieapvyhrw.supabase.co/storage/v1/object/public/system/system/logo.png"
                 alt="System Logo"
-                className="h-40 w-40 object-contain"
+                className="h-80 w-80 object-contain"
               />
             </div>
             
